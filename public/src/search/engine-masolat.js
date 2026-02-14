5612dcf838f2c65d80df470aaae76f934f9d96ed
fodlacus <98264839+fodlacus@users.noreply.github.com>
2026-02-15 00:51:09 +0100

    új keresés blokk bekötése 13

--- public/src/search/engine-masolat.js
+++ public/src/search/engine-masolat.js
@@ -1,5 +1,4 @@
-//import { adatbazis } from "./firebase-config.js";
-import { adatbazis } from "../../js/util/firebase-config.js";
+import { adatbazis } from "./../js/util/firebase-config.js";
 import {
   collection,
   getDocs,
